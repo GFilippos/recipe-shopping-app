6707c519eebb0f2f9bd5131de1670ff1eabb8f5e
GFilippos <gomelisfil@gmail.com>
2023-10-02 23:31:21 +0300

    finilize changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,24 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg'
     ),
+    new Recipe(
+      'Another Test Recipe',
+      'Second Test',
+      'https://insanelygoodrecipes.com/wp-content/uploads/2020/12/Eggs-Benedict-with-Herbs.png'
+    ),
   ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeClick(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
