17842784af30eb7cf0b71a806a8f6ea692a3f0dc
GFilippos <gomelisfil@gmail.com>
2023-10-02 22:30:45 +0300

    changes on data-binding

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css'],
 })
 export class RecipesComponent implements OnInit {
+  recipesView = false;
   constructor() {}
 
   ngOnInit(): void {}
